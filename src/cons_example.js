73cd6e2307c06f48d9154f9692e7d9fa4543920b
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2021-01-26 16:38:26 -0500

    Create cons_example.js

---
+++ src/cons_example.js
@@ -0,0 +1,7 @@
+//gate.io keys
+const AK = "23456789045678";
+const SK = "frtgyhde456789ed567s89dfbhnjmefs";
+// wallet app key
+const WO = "ctvbghnjmk5567889090657890'";
+
+export default {AK, SK, WO};
