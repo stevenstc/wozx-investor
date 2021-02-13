c54e21c1dd58a0eca6dc7bf1a59ea6a16bba1d18
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2021-02-13 15:11:22 -0500

    Final Deploy

---
+++ docs/precache-manifest.c3b03643b89b6c758e27d5e182132af9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e37d16bb58e8bc90830cc2845d6e4370",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19b5cebb723fac8cdb72",
+    "url": "/static/js/2.14253e1d.chunk.js"
+  },
+  {
+    "revision": "4d92265ee4745234cbf941e19ca6c02e",
+    "url": "/static/js/2.14253e1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e627b27c8ca956f8b76e",
+    "url": "/static/js/main.7c68aab4.chunk.js"
+  },
+  {
+    "revision": "5cc8d2ec213dfdc1c6af",
+    "url": "/static/js/runtime-main.b78fadcc.js"
+  },
+  {
+    "revision": "f8e7d99ae8f0718500322e6fbd5f9ee2",
+    "url": "/static/media/TronLinkLogo.f8e7d99a.png"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wozx-investor/precache-manifest.620968e66d8383a2761fe72b2d044355.js"
+  "/precache-manifest.c3b03643b89b6c758e27d5e182132af9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wozx-investor/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
