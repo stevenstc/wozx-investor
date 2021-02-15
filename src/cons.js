2156cebaaff86d237d466b210303a221dfec42f8
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2021-02-15 16:20:56 -0500

    FINAL 07

--- src/cons.js
+++ src/cons.js
@@ -1,9 +1,5 @@
 import {REACT_APP_AK, REACT_APP_SK, REACT_APP_WO, REACT_APP_proxy, REACT_APP_PRU, REACT_APP_WS, REACT_APP_descuento, REACT_APP_WOZX, REACT_APP_TRX, REACT_APP_SC} from "@env"
 
-//require('dotenv').config();
-
-//console.log(REACT_APP_proxy);
-
 const AK = REACT_APP_AK;
 const SK = REACT_APP_SK;
 const WO = REACT_APP_WO;
