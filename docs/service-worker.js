244a0255ee076ec2fe7df26a50c7dd9f91668cad
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2021-02-20 18:29:51 -0500

    si buenas

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wozx-investor/precache-manifest.ce4a42f2c27f3058c0d2cf30d799ca45.js"
+  "/wozx-investor/precache-manifest.bd87d3cb7a4ba748430ba74f87c996ef.js"
 );
 
 self.addEventListener('message', (event) => {
